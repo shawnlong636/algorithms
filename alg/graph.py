2620ed267ae428c35a26a99d9b4cb02b429b55db
Shawn Long <slong024@ucr.edu>
2021-11-13 11:16:38 -0800

    Added DisjointSet class to graph.py

--- alg/graph.py
+++ alg/graph.py
@@ -38,3 +38,32 @@ def flood_fill(table2D: [[int]], start_idx: (int, int) = (0,0), new_val: int = 9
 
 
     return table2D
+
+class DisjointSet:
+	set_map = {}
+
+	def __init__(self, nodes: [int] = []):
+		self.set_map = {node: node for node in nodes}
+
+	def from_graph(self, graph: nx.Graph):
+		self.set_map = {node: node for node in graph.nodes()}
+
+	def find(self, node1, node2) -> bool:
+		if not node1 in self.set_map:
+			raise ValueError(f'Node: {node1} not in set')
+
+		if not node2 in self.set_map:
+			raise ValueError(f'Node: {node2} not in set')
+
+		if self.set_map.get(node1) == self.set_map.get(node2):
+			return True
+
+		else:
+			return False
+
+	def union(self, node1: int, node2: int):
+		if not self.find(node1, node2):
+			self.set_map[node2] = self.set_map[node1]
+
+	def __str__(self):
+		return str(self.set_map)
