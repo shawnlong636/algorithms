ceb2840242aeb44886206c14d1122a3b817dbd3f
Shawn Long <slong024@ucr.edu>
2021-11-20 18:40:26 -0800

    Moved all test cases for parallel.Sum() into one test method

--- test/test_parallel.py
+++ test/test_parallel.py
@@ -3,18 +3,15 @@ from alg import parallel
 import numpy as np
 
 class TestParallelMethods(unittest.TestCase):
-    def test_tiny_sum(self):
+    def test_Parallel_Sum(self):
         nums = np.random.randint(0,1000, 50)
         self.assertEqual(parallel.Sum(nums), sum(nums))
 
-    # def test_small_sum(self):
-    #     nums = np.random.randint(-1 * (10 ** 3), 10 ** 3, 10 ** 3)
-    #     self.assertEqual(parallel.Sum(nums), sum(nums))
+        # nums = np.random.randint(-1 * (10 ** 3), 10 ** 3, 10 ** 3)
+        # self.assertEqual(parallel.Sum(nums), sum(nums))
 
-    # def test_med_sum(self):
-    #     nums = np.random.randint(-1 * (10 ** 3), 10 ** 3, 10 ** 3)
-    #     self.assertEqual(parallel.Sum(nums), sum(nums))
+        # nums = np.random.randint(-1 * (10 ** 3), 10 ** 3, 10 ** 3)
+        # self.assertEqual(parallel.Sum(nums), sum(nums))
 
-    # def test_big_sum(self):
-    #     nums = np.random.randint(-1 * (10 ** 6), 10 ** 6, 10 ** 6)
-    #     self.assertEqual(parallel.Sum(nums), sum(nums))
+        # nums = np.random.randint(-1 * (10 ** 6), 10 ** 6, 10 ** 6)
+        # self.assertEqual(parallel.Sum(nums), sum(nums))
